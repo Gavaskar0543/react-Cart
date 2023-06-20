263ec66662051a9b654d8cf03c3cee86d656e119
Gavaskar <gavaskaraeg@gmail.com>
2023-06-20 15:38:21 +0530

    firebase setup done

--- src/App.js
+++ src/App.js
@@ -4,37 +4,29 @@ import React from 'react';
 import Cart from './cart'
 import './index.css';
 import Navbar from './Navbar';
-
+import { initializeApp }  from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore'
+import { FirebaseError } from 'firebase/app';
 class  App extends React.Component {
   constructor(){
     super();
     
     this.state = {
-    products:[
-        {
-        title:'phone',
-        price:999,
-        qty:0,
-        img:'https://images.unsplash.com/photo-1525598912003-663126343e1f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
-        id:1
-        },
-        {
-            title:'watch',
-        price:99,
-        qty:0,
-        img:'https://images.unsplash.com/photo-1544117519-31a4b719223d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=452&q=80',
-        id:2
-        }
-        ,{
-            title:'laptop',
-        price:9999,
-        qty:0,
-        img:'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=820&q=80',
-        id:3
-        }
-    ]
+    products:[ ]
 }
 
+}
+componentDidMount(){
+  const db = getFirestore();
+  const productCollectionRef = collection(db, 'product');
+
+  getDocs(productCollectionRef)
+    .then((snapshot) => {
+      console.log(snapshot);
+    })
+    .catch((error) => {
+      console.log('Error getting documents:', error);
+    });
 }
 handleIncreaseQty = (product) => {
 //console.log('hello you call me to increase qty!',product);
--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { initializeApp } from 'firebase/app';
 
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCokWNF1bO9qtH1byh0LERU1rEZ6y3vGK4",
+  authDomain: "cart-c9bfd.firebaseapp.com",
+  projectId: "cart-c9bfd",
+  storageBucket: "cart-c9bfd.appspot.com",
+  messagingSenderId: "484629330025",
+  appId: "1:484629330025:web:ea707018948a9fd9f74af8"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
